fb864cd5e6beab28c432d8f3e1dca3abe55b923b
Toxblh <toxblh@gmail.com>
2019-08-01 13:57:47 +0100

    simple test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,16 @@
+import Samsung from './index'
+
 describe('sum', () => {
-  it('sums two numbers', () => {
-    expect(1 + 2).toEqual(3)
+  it('Successful create instance', () => {
+    const config = {
+      name: 'NodeJS-Test', // Default: NodeJS
+      ip: '192.168.1.2',
+      mac: '123456789ABC',
+      token: '12345678',
+      debug: true // Default: false
+    }
+
+    const control = new Samsung(config)
+    expect(control).toBeInstanceOf(Samsung)
   })
 })
